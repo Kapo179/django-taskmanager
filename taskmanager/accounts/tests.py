0432b62bdb4f8818cdd20a332765970a71ddd1c3
Abdirahman Abdi <ABDIRAHMAN.ABDI1273@GMAIL.COM>
2025-01-16 23:02:00 +0000

    Test: Added Account model tests function (user creation with attributes functionality  & admin roles) added in accounts/tests

--- taskmanager/accounts/tests.py
+++ taskmanager/accounts/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserTests(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='12345',
+            email='test@example.com',
+            role='regular'
+        )
+
+    def test_user_creation(self):
+        """Test that a user creation with below attributes is successful"""
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.email, 'test@example.com')
+        self.assertEqual(self.user.role, 'regular')
+        self.assertTrue(self.user.is_active)
+        self.assertFalse(self.user.is_staff)
+
+    def test_user_role_choices(self):
+        """Test that user roles are limited to defined choices"""
+        admin_user = get_user_model().objects.create_user(
+            username='adminuser',
+            password='12345',
+            role='admin'
+        )
+        self.assertEqual(admin_user.role, 'admin')
