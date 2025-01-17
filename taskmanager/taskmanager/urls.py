3d161859e5f16277e0879d161380982b4dafdb17
Abdirahman Abdi <ABDIRAHMAN.ABDI1273@GMAIL.COM>
2025-01-17 10:20:15 +0000

    Accounts: Signup functionality setup which will allow users to register new accounts, HTML setup next

--- taskmanager/taskmanager/urls.py
+++ taskmanager/taskmanager/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views as account_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('boards.urls')),
+    path('signup/', account_views.signup, name='signup'),
 ]
