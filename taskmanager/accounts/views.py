0ad6f71ec4e427207de2e60e1402d96bc7594524
Abdirahman Abdi <ABDIRAHMAN.ABDI1273@GMAIL.COM>
2025-01-17 11:34:51 +0000

    Django Test fix: Signup.html now setup for our New Signup functionality, this allows new User sign-up. Login Page updated, with a sign-up button to allow new signups

--- taskmanager/accounts/views.py
+++ taskmanager/accounts/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('board')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
